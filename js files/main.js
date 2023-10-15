1493ec54138de2c8be705496a15c76c2163ea181
MoustafaFikry <moustafafikry401@gmail.com>
2023-10-15 23:20:50 +0200

    add project

---
+++ js files/main.js
@@ -0,0 +1,12 @@
+let mainH2 = document.querySelector(".header h2");
+mainH2.style.cssText = "cursor: pointer";
+
+mainH2.onclick = function () {
+    window.location.reload()
+}
+
+let up = document.querySelector(".banner .up")
+
+
+
+
